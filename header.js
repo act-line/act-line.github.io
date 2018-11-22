8e5cb9d8efd543fe169d0ed0b893de8a4102bd9f
chun <chun.jb.37@gmail.com>
2018-11-22 15:10:57 +0900

    デザイン変更 (#2)
    * all renewal/change design

--- header.js
+++
@@ -1,6 +0,0 @@
-var head = document.createElement('head');
-head.innerHTML = '<link rel="stylesheet" href="css/set.css" type="text/css" />';
-head.innerHTML = '<link rel="stylesheet" type="text/css" href="css/back.css">';
-head.innerHTML = '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>';
-head.innerHTML = '<script type="text/javascript" src="js/set.js"></script> ';
-document.head.appendChild(head);
--- header_and_footer.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-            $("#header").load("template.html");
-            $("#footer").load("footer.html");
-          });
